6be7da888671f66d910bd10ac7537e04c384f250
Rakibul Hasan <rakibul.hasan.967539@gmail.com>
2022-08-28 12:54:23 +0600

    new profile picture added and some code changed

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Web Developer",  " Web Designer", "Youtuber"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", " Web Designer", "Youtuber"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
